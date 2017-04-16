0526267eff3667d44b3d04fce77849dbb5de8b9a
YixinZhang <hxy820718@163.com>
2017-04-16 21:25:51 +1200

    implement accommodation filter
    implement accommodation filter

--- queensTown/js/global.js
+++ queensTown/js/global.js
@@ -2,22 +2,27 @@ function toggle(targetid){
 
     if(targetid == "all")
     {
-        var showItems = new Array('hotel1','hotel2','bb1','motel1');
+        var showItems = new Array('hotel1','hotel2','hotel3','bb1','bb2','bb3','motel1','motel2','lodge1','lodge2');
     }
     else if(targetid == "hotels")
     {
-    	var showItems = new Array('hotel1','hotel2');
-        var hideItems = new Array('bb1','bb2','motel1');
+    	var showItems = new Array('hotel1','hotel2','hotel3');
+        var hideItems = new Array('bb1','bb2','bb3','motel1','motel2','lodge1','lodge2');
     }
     else if(targetid == "motels")
     {
-        var showItems = new Array('motel1');//if there is no 'bb2',this won't work
-        var hideItems = new Array('hotel1','hotel2','bb1','bb2','bb3');	
+        var showItems = new Array('motel1','motel2');//if there is no 'bb2',this won't work
+        var hideItems = new Array('hotel1','hotel2','hotel3','bb1','bb2','bb3','lodge1','lodge2');	
+    }
+    else if(targetid == "lodges")
+    {
+        var showItems = new Array('lodge1','lodge2' );//if there is no 'bb2',this won't work
+        var hideItems = new Array('hotel1','hotel2','hotel3','bb1','bb2','bb3','motel1','motel2');  
     }
     else
     {
-    	var showItems = new Array('bb1','bb2');//if there is no 'bb2',this won't work
-        var hideItems = new Array('hotel1','hotel2','motel1');	
+    	var showItems = new Array('bb1','bb2','bb3');//if there is no 'bb2',this won't work
+        var hideItems = new Array('hotel1','hotel2','hotel3','motel1','motel2','lodge1','lodge2');	
     }
 
     for(var key of showItems){
