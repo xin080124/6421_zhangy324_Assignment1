0ee4bfb6f43d841c9800f677e9b2ebb82c691f08
nancyUnitec <xin080124@hotmail.com>
2017-06-18 15:53:03 +1200

    enable google map on about webpage
    move js function from jquery to global.js

--- Assignment/js/global.js
+++ Assignment/js/global.js
@@ -267,3 +267,4 @@ function EmailCheck()
     return true; 
 } 
 
+function initMap() { var uluru = {lat: -36.851499, lng: 174.765302}; var map = new google.maps.Map(document.getElementById('map'), { zoom: 16, center: uluru }); var marker = new google.maps.Marker({ position: uluru, map: map }); }
