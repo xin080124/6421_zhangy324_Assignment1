35844048633d4023c03961f6ed1b28b16a371e6c
Nancy Zhang <nancyzhang@Nancys-MacBook-Air.local>
2017-08-17 23:35:43 +1200

    use lecturer's code

--- IpadDraw1483998/IpadDraw1483998/ViewController.swift
+++ IpadDraw1483998/IpadDraw1483998/ViewController.swift
@@ -1,15 +1,15 @@
 //
 //  ViewController.swift
-//  IpadDraw1483998
+//  PanGesture
 //
-//  Created by Nancy Zhang on 17/08/17.
-//  Copyright © 2017 Nancy Zhang. All rights reserved.
+//  Created by Christine Jiang on 16/08/17.
+//  Copyright © 2017 Christine Jiang. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
     var startPoint: CGPoint = CGPoint.zero
     var endPoint: CGPoint = CGPoint.zero
     var customPath : UIBezierPath?
@@ -18,18 +18,20 @@ class ViewController: UIViewController {
     var lineCap:String = kCALineCapRound
     let shapeArray = [Shapes.oval, Shapes.rectangle, Shapes.line, Shapes.freeStyle]
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
-    @IBAction func move(_ sender: UIPanGestureRecognizer) {
+    @IBAction func handlePan(_ sender: UIPanGestureRecognizer)
+    {
         if sender.state == .began
         {
             customPath = UIBezierPath()
@@ -68,7 +70,27 @@ class ViewController: UIViewController {
         }
     }
     
-    
-
+    @IBAction func shapeDidSelect(_ sender: UIButton)
+    {
+        //The code commented is equal to the code below
+        //        let selectedTag : Int = sender.tag
+        //        switch selectedTag
+        //        {
+        //        case 0:
+        //            selectedShape = Shapes.oval
+        //
+        //        case 1:
+        //            selectedShape = Shapes.rectangle
+        //
+        //        case 2:
+        //            selectedShape = Shapes.line
+        //
+        //        case 3:
+        //            selectedShape = Shapes.freeStyle
+        //
+        //        default:
+        //            selectedShape = Shapes.oval
+        //        }
+        selectedShape = shapeArray[sender.tag]
+    }
 }
-
