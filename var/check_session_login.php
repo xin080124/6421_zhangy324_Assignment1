5cd888c6e1ccf8f5e614d1c2a1dd87306b3fe13d
nancyUnitec <xin080124@hotmail.com>
2017-09-08 20:37:38 +1200

    pass book id to a new php page

--- var/check_session_login.php
+++ var/check_session_login.php
@@ -7,17 +7,18 @@ or die("data base selected failed");
 $username = $_POST['username'];
 $passcode = $_POST['passcode'];
 //获取session的值
-$query = @mysql_query("select username,userflag from users where username = '$username' and passcode = '$passcode'")
+$query = @mysql_query("select username,userflag,ID from users where username = '$username' and passcode = '$passcode'")
 or die("SQL语句执行失败");
 //判断用户以及密码
 if($row = mysql_fetch_array($query))
 {
-	echo $row;
+	echo $row['ID'];
     session_start();
     //判断权限
     if($row['userflag'] == 1 or $row['userflag'] == 0 or $row['userflag'] == 2){
         $_SESSION['username'] = $row['username'];
         $_SESSION['userflag'] = $row['userflag'];
+		$_SESSION['userID'] = $row['ID'];
         echo "<a href='welcome_session_login.php'>欢迎访问www.cnblogs.com/txw1958</a>";
     }else{
         echo "userflag不正确";
