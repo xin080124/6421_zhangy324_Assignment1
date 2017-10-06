a85b5b9ab36c4dba879ce590bc602cb0eb442859
nancyUnitec <xin080124@hotmail.com>
2017-10-06 17:22:11 +1300

    add features of enrolling and removing student to a course

--- var/studentList.php
+++ var/studentList.php
@@ -57,6 +57,7 @@ or die("data base selected failed");
 
 //$userId = $_SESSION['userID'];
 $studentID = $_SESSION['userID'];
+$userFlag = $_SESSION['userflag'];
 
 $query = @mysql_query("select person_id, course_id from person_courses where person_id = $studentID ")or die("SQL failed");
 
@@ -68,6 +69,61 @@ echo "</br></br>";
 $courseID = 0;
 //if($row = mysql_fetch_array($query))
 //echo $row[1];
+
+//after recommend action
+if(isset($_GET["rcid"]))
+{
+	//echo "from recommend action";
+    $recommend_course=$_GET["rcid"];
+    $courseID = $_GET["rcid"];
+}
+if(isset($_GET["rbid"]))
+{
+	//echo "rrrrrrrrr";
+    $studentID = $_GET['rbid'];
+	echo $studentID;
+	echo "</br>";
+	if($userFlag == 1)
+	{
+	    $sql = "INSERT INTO person_courses (person_id, course_id) VALUES (".$studentID.", ".$courseID.")";
+	}
+    else
+    {
+		$sql = "INSERT INTO student_courses_books (person_id, course_id, book_id) VALUES (".$userId.", ".$courseID.", ".$bookID.")";
+	}	
+	echo $sql;		
+    @mysql_query($sql)or die(" SQL failed");
+}
+
+if(isset($_GET["dcid"]))
+{
+	//echo "from delete action";
+    $recommend_course=$_GET["dcid"];
+    $courseID = $_GET["dcid"];
+}
+if(isset($_GET["dbid"]))
+{
+	//echo "ddddddd";
+    $studentID = $_GET['dbid'];
+	echo $studentID;
+	//$sql = "INSERT INTO recommend_courses_books (person_id, course_id, book_id) VALUES (".$userId.", ".$courseID.", ".$bookID.")";
+    echo "</br>";
+	if($userFlag == 1)
+	{
+		$sql = "DELETE FROM person_courses WHERE
+		person_id = $studentID and course_id = $courseID";
+	}
+	else
+	{
+		$sql = "DELETE FROM student_courses_books  WHERE
+		person_id = $userId and course_id = $courseID and book_id = $bookID";
+	}
+	echo $sql;		
+    @mysql_query($sql)or die(" SQL failed");
+}
+
+
+
 ?>
     </div>
     <div class="col-sm-4">
@@ -76,6 +132,11 @@ $courseID = 0;
   </div>
 </div>
 <?php
+
+echo "</br></br></br>";
+echo "<h2> <a href=\"courseList.php\">My courses</a></h2>";
+
+
 while($row = mysql_fetch_array($query))
 {
 	echo "<div class=\"col-sm-4 col-xs-12\"> ";
@@ -100,24 +161,114 @@ while($row = mysql_fetch_array($query))
 	//echo "</br>";
 	//$linkStr = "<a href='bookView.php?id=".$bookID."&name=".$bookName." '>".$res['book_name'];
 	    echo "<div class=\"panel-body\">";
-	$courseLink = "<h2><a href='courseView.php?id=".$courseID."&name=".$courseName." '>".$courseName."</a>";
-	echo $courseLink;
+	//$courseLink = "<h2><a href='courseView.php?id=".$courseID."&name=".$courseName." '>".$courseName."</a>";
+	//echo $courseLink;
 	echo "</div>";
 	//$example = "<a href='bookView.php?id=19 '>click me to jump</a>";
 	//echo $example;
 	
-	showCourseBooks($studentID,$courseID);
+	//showCourseBooks($studentID,$courseID);
+
 	echo "</div>";
 	echo "</div>";
 	
+	/*
 	if($userFlag == 1)
 	{
 	    $studentLink = "<h2><a href='courseView.php?id=".$courseID."&name=".$courseName." '>student management</a>";
 	echo $studentLink;
 		
 	}
-	
+	*/
 }	
+
+showStudentEnrolled($courseID);
+
+showStudentNotEnrolled($courseID);
+
+function showStudentEnrolled($course_id)
+{
+	$sql = "select users.ID, users.username from person_courses,  users where
+	users.ID = person_courses.person_id
+	and
+	person_courses.course_id = $course_id";
+    echo "</br>";
+	echo $sql;
+	
+	$query = @mysql_query($sql)or die("SQL failed");
+	
+	while($row = mysql_fetch_array($query))
+	{
+		echo "<div class=\"col-sm-4 col-xs-12\"> ";
+	    echo "<div class=\"panel panel-default text-center\">";
+        echo "<div class=\"panel-heading\">";
+        echo "<h2>enrolled</h2>";
+        echo "</div>";
+		 //$bookID = $row['book_id'];
+		 //$bookName = $row['book_name'];
+		 $studentID = $row['ID'];
+		 $studentName = $row['username'];
+		 echo "</br>";
+		 echo $studentID;
+		 echo "</br>";
+		 echo $studentName;
+		 echo "<form method=\"POST\" action=\"studentList.php?dbid=".$studentID."&dcid=".$course_id."\"></br><input type=\"submit\" name=\"Submit\" value=\"remove\" /></form></br>";
+		 echo "</div>";
+		 echo "</div>";
+	}	
+
+
+}
+
+function showStudentNotEnrolled($course_id)
+{
+	
+    echo "</br>";
+	echo "showStudentNotEnrolled";
+	echo "</br>";
+	
+	$sql = "select users.ID, users.username from person_courses,  users where
+	users.ID = person_courses.person_id
+	and
+	person_courses.course_id = $course_id";
+	
+	
+	$sql = "select users.ID, users.username from users where
+	users.ID not in
+	(
+	    select person_courses.person_id from person_courses
+		where person_courses.course_id = $course_id
+	)
+	";
+	
+    echo "</br>";
+	echo $sql;
+	
+	$query = @mysql_query($sql)or die("SQL failed");
+	
+	while($row = mysql_fetch_array($query))
+	{
+		echo "<div class=\"col-sm-4 col-xs-12\"> ";
+	    echo "<div class=\"panel panel-default text-center\">";
+        echo "<div class=\"panel-heading\">";
+        echo "<h2>Other Students</h2>";
+        echo "</div>";
+		 //$bookID = $row['book_id'];
+		 //$bookName = $row['book_name'];
+		 $studentID = $row['ID'];
+		 $studentName = $row['username'];
+		 echo "</br>";
+		 echo $studentID;
+		 echo "</br>";
+		 echo $studentName;
+		 echo "<form method=\"POST\" action=\"studentList.php?rbid=".$studentID."&rcid=".$course_id."\"></br><input type=\"submit\" name=\"Submit\" value=\"enroll\" /></form></br>";
+		 echo "</div>";
+		 echo "</div>";
+	}	
+
+
+}
+
 
 function showCourseBooks($student_id,$course_id)
 {
